af39484105a7cd36d4f1b68fa7a5f3e159db1203
Mateen PC <muhammadumairali987@gmail.com>
2023-03-06 22:43:57 +0500

    06032023

--- app/src/main/java/com/example/viewpagerexample/MainActivity.kt
+++ app/src/main/java/com/example/viewpagerexample/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.viewpagerexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPager = findViewById<ViewPager>(R.id.viewPager)
+        viewPager.adapter = PageAdapter(supportFragmentManager)
+
+        val tabLayout = findViewById<TabLayout>(R.id.tabs)
+        tabLayout.setupWithViewPager(viewPager)
     }
 }
---
+++ app/src/main/java/com/example/viewpagerexample/PageAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.viewpagerexample
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PageAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 3;
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when(position) {
+            0 -> {
+                return OneFragment
+            }
+            1 -> {
+                return FragmentTwo
+            }
+            2 -> {
+                return Fragmenthree
+            }
+            else -> {
+                return OneFragment
+            }
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when(position) {
+            0 -> {
+                return "Tab 1"
+            }
+            1 -> {
+                return "Tab 2"
+            }
+            2 -> {
+                return "Tab 3"
+            }
+        }
+        return super.getPageTitle(position)
+    }
+
+}
